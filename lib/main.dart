5bbe86ed638b3682b66aad26b3ea905cdd22a597
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2020-09-29 17:18:07 -0500

    Provider Setup and Cupertino widgets setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,61 @@
+// Dart imports
+import 'dart:io';
+
+// Package imports
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// Widget Imports
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => Hola(),
-      },
-    );
-  }
-}
-
-class Hola extends StatelessWidget {
-  const Hola({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Hola')),
-      body: Center(
-        child: Text('Que tal'),
-      ),
-    );
+    return ChangeNotifierProvider.value(
+        value: GreatPlaces(),
+        child: Platform.isIOS
+            ? CupertinoApp(
+                title: 'Great Places',
+                debugShowCheckedModeBanner: false,
+                theme: CupertinoThemeData(
+                  primaryColor: Colors.white,
+                  primaryContrastingColor: CupertinoColors.systemIndigo,
+                  barBackgroundColor: CupertinoColors.systemIndigo,
+                  textTheme: CupertinoTextThemeData(
+                    navTitleTextStyle: TextStyle(
+                      color: CupertinoColors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    navLargeTitleTextStyle: TextStyle(
+                      fontFamily: 'Cupertino',
+                      color: CupertinoColors.white,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                initialRoute: '/',
+                routes: {
+                  '/': (ctx) => PlacesListScreen(),
+                },
+              )
+            : MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Great Places',
+                theme: ThemeData(
+                  primarySwatch: Colors.indigo,
+                  accentColor: Colors.amber,
+                  visualDensity: VisualDensity.adaptivePlatformDensity,
+                ),
+                initialRoute: '/',
+                routes: {
+                  '/': (ctx) => PlacesListScreen(),
+                },
+              ));
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,23 +1,91 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Places'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {},
+    final pageBody = Platform.isIOS
+        ? NestedScrollView(
+            headerSliverBuilder: (BuildContext ctx, bool innerBoxIsScrolled) {
+              return <Widget>[
+                CupertinoSliverNavigationBar(
+                  largeTitle: Text('Your Places'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      GestureDetector(
+                        child: Icon(CupertinoIcons.add),
+                        onTap: () {
+                          showCupertinoDialog(
+                            context: ctx,
+                            builder: (_) => CupertinoAlertDialog(
+                              title: Text('Hey!'),
+                              content: Text('Do you like cheese?'),
+                              actions: [
+                                CupertinoDialogAction(
+                                  child: Text('Yes'),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                CupertinoDialogAction(
+                                  child: Text('No'),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ];
+            },
+            body: Center(
+              child: CupertinoActivityIndicator(),
+            ),
           )
-        ],
-      ),
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+        : Center(
+            child: CircularProgressIndicator(),
+          );
+
+    final pageBar = Platform.isIOS
+        ? CupertinoNavigationBar(
+            middle: Text('Your Places'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  child: Icon(CupertinoIcons.add),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          )
+        : AppBar(
+            title: Text('Your Places'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {},
+              )
+            ],
+          );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: pageBody,
+          )
+        : Scaffold(
+            appBar: pageBar,
+            body: pageBody,
+          );
   }
 }
