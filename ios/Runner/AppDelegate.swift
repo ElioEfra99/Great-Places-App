546b75ea92c14eff033626d9ae2cbfe0c39c4fb3
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2020-11-14 19:43:35 -0600

    Handling for Google maps api key implemented

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,22 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    var config: [String:String]
+    if let infoPlistPath = Bundle.main.url(forResource: "Secrets", withExtension: "plist") {
+        do {
+            let infoPlistData = try Data(contentsOf: infoPlistPath)
+            if let dict = try PropertyListSerialization.propertyList(from: infoPlistData, options: [], format: nil) as? [String: String] {
+                config = dict
+                GMSServices.provideAPIKey(config["GOOGLE_API_KEY"]!)
+            }
+//            if let dict = try PropertyListSerialization.propertyList(from: infoPlistPath, options: [], format: nil) as? [String:String] {
+//                config = dict
+//                GMSServices.provideAPIKey(config["GOOGLE_API_KEY"])
+//            }
+        } catch {
+            print(error)
+        }
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
